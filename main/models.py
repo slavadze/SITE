b0041330debe3f8523b99e15f462134af3ffdb3c
slavadze <6354045@mail.ru>
2021-04-28 19:56:09 +0300

    create model Product

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+
+    available_CHOICES = (
+        ('1', 'В наличии'),
+        ('2', 'Под заказ'),
+    )
+
+    title = models.CharField(max_length=200)
+    price = models.IntegerField()
+    description = models.TextField()
+    currency = models.CharField(max_length=3)
+    availability = models.CharField(max_length=30, choices=available_CHOICES, default='1')
+    img = models.ImageField(upload_to='static/images/', null=True, blank=True, default='static/images/default.jpg')
