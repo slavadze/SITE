c989e979eb9011ab4e78b72423a9aeb40c3afa34
slavadze <6354045@mail.ru>
2021-04-30 07:52:27 +0300

    create catalog.html

--- catalog/urls.py
+++ catalog/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from main.views import *
 
 urlpatterns = [
+    path('', catalogPage),
     path('', indexPage),
     path('admin/', admin.site.urls),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import *
+
 
 def indexPage(requests):
     return render(requests, 'index.html')
+
+
+def catalogPage(requests):
+    products = Product.objects.all()
+    return render(requests, 'catalog.html', {'products': products})
